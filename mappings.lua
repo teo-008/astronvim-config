ef0738dcc57dfeb88decf88d3bfce962039bb6ac
Matthew Curcio <matt@dittoflo.co>
2023-04-04 10:59:48 +1000

    initial customisation

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,10 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
@@ -19,7 +22,54 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "[S]ave File" },
+    -- harpoon
+    ["<leader>ba"] = {
+      require('harpoon.mark').add_file,
+      desc = '[A]dd file'
+    },
+    ["<leader>bq"] = {
+      require('harpoon.ui').toggle_quick_menu,
+      desc = '[T]oggle quick menu'
+    },
+    ["<leader>bk"] = {
+      function() require('harpoon.ui').nav_next() end,
+      desc = 'Nav to next file'
+    },
+    ["<leader>bj"] = {
+      function() require('harpoon.ui').nav_prev() end,
+      desc = 'Nav to previous file'
+    },
+    ["<leader>br"] = {
+      require('harpoon.mark').rm_file,
+      desc = '[R]emove file'
+    },
+    ["<leader>bf"] = {
+      function()
+        local count = vim.v.count > 0 and vim.v.count or 1
+        require('harpoon.ui').nav_file(count)
+      end,
+      desc = 'Nav to specific file'
+    },
+    -- projects
+    ["<leader>fp"] = { "<CMD>Telescope projects<CR>", desc = "[F]ind [P]rojects" },
+    -- spider
+    ["w"] = { "<cmd>lua require('spider').motion('w')<CR>", desc = "Spider-w" },
+    ["e"] = { "<cmd>lua require('spider').motion('e')<CR>", desc = "Spider-w" },
+    ["b"] = { "<cmd>lua require('spider').motion('b')<CR>", desc = "Spider-w" },
+    ["ge"] = { "<cmd>lua require('spider').motion('ge')<CR>", desc = "Spider-w" },
+  },
+  o = {
+    ["w"] = { "<cmd>lua require('spider').motion('w')<CR>", desc = "Spider-w" },
+    ["e"] = { "<cmd>lua require('spider').motion('e')<CR>", desc = "Spider-w" },
+    ["b"] = { "<cmd>lua require('spider').motion('b')<CR>", desc = "Spider-w" },
+    ["ge"] = { "<cmd>lua require('spider').motion('ge')<CR>", desc = "Spider-w" },
+  },
+  x = {
+    ["w"] = { "<cmd>lua require('spider').motion('w')<CR>", desc = "Spider-w" },
+    ["e"] = { "<cmd>lua require('spider').motion('e')<CR>", desc = "Spider-w" },
+    ["b"] = { "<cmd>lua require('spider').motion('b')<CR>", desc = "Spider-w" },
+    ["ge"] = { "<cmd>lua require('spider').motion('ge')<CR>", desc = "Spider-w" },
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,11 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  { import = "astrocommunity.project.project-nvim" },             -- project manager
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" }, -- rainbow parentheses
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+
+  -- motion
+  { import = "astrocommunity.motion.leap-nvim" }
 }
